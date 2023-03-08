81833b5139d2e3c7ae8d0b09491db9cd685de504
chen yao <444722407@qq.com>
2023-03-08 11:57:06 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,16 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
 # deploy to github
-# 如果$GITHUB_TOKEN环境变量字符串 长度为0则为true  本地长度为0
-echo "test----------"
-
-echo $GITHUB_TOKEN
-echo [-z "$GITHUB_TOKEN"]
-
-echo "--------test"
+# 如果$GITHUB_TOKEN环境变量字符串 长度为0则为true  本地长度为0 线上打包环节为保密的值不为0
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:444722407/blog.git
 else
+  # 生成静态文件
+  npm run docs:build
+  # 进入生成的文件夹
+  cd docs/.vuepress/dist
   msg='来自github action的自动部署'
   githubUrl=https://44722407:${GITHUB_TOKEN}@github.com/444722407/444722407.github.io.git
   git config --global user.name "chenyao"
