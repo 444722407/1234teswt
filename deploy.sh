e7ce63628fb42a9b172590d2568b85ac58f001d9
chen yao <444722407@qq.com>
2023-03-08 12:00:43 +0800

    deploy-local

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,14 @@ set -e
 # deploy to github
 # 如果$GITHUB_TOKEN环境变量字符串 长度为0则为true  本地长度为0 线上打包环节为保密的值不为0
 if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
+  msg='deploy-local'
   githubUrl=git@github.com:444722407/blog.git
 else
   # 生成静态文件
   npm run docs:build
   # 进入生成的文件夹
   cd docs/.vuepress/dist
+  
   msg='来自github action的自动部署'
   githubUrl=https://44722407:${GITHUB_TOKEN}@github.com/444722407/444722407.github.io.git
   git config --global user.name "chenyao"
@@ -22,7 +23,7 @@ fi
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m $msg
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f $githubUrl  master
